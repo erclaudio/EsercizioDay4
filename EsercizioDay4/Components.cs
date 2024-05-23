450df5ed984ba69d7f3e503f189490c5b7d36f72
erclaudio <erfattone@gmail.com>
2024-05-23 11:10:24 +0100

    Add project files.

---
+++ EsercizioDay4/Components.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioDay4
+{
+    public class Components
+    {
+        public void printMessages(int message) 
+        {
+            if (message == 1) // if statement
+            {
+                Console.WriteLine("Enter the Amount of sheep the shepard needs to watch over:");
+            }
+            switch (message) //switch statement
+            {
+                case 2:
+                    Console.WriteLine("Enter the Amount of space the shepard has available (Km2):");
+                    break;
+                case 3:
+                    Console.WriteLine($"The shepard will need {NrOfDogs(Program.nrOfSheep)} dogs"); 
+                    break;
+                case 4:
+                    Km2(new Program().spaceAvailable);
+                    break;
+                default:
+                    break;
+            } 
+
+        }
+        public static double NrOfDogs(double sheep) //static returns double
+        {
+            double DogsAmt = Math.Ceiling((sheep-50) / 50);
+            return DogsAmt;
+            
+        }
+        
+        public static void Km2(double space) //static returns nothing
+        {
+            double available = Program.nrOfSheep / space;
+            if((Program.nrOfSheep / space) > 50.0)
+            {
+                Console.WriteLine("The shepard does not have enough space");                
+            }
+            else if((Program.nrOfSheep / space) <= 50.0 )
+            {
+                Console.WriteLine("The shepard has enough space");
+            }
+            
+
+        }
+
+    }
+}
---
+++ EsercizioDay4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioDay4
+    /*
+     * Assume a shepard wants a system to understand how many sheep he can keep under control at once. 
+     * He tells you he can watch over 50 sheep in an area of 1km squared. 
+     * He can however use dogs to increase the number of sheeps he can watch. 
+     * 1 dog adds 50 sheep per sqare kilomenter.
+     * He receives requests with the number of sheep he needs to watch over and the amount of space he has available.
+     * Write a program to understand if he needs any dogs to watch over the sheep and how many dogs he needs.
+     * Assume that to watch more than 50 sheep he needs a new dog each time.
+     * Assume no more than 50 sheep fit in 1 square km.
+     */
+{
+    
+    public class Program
+    {
+
+        public static int nrOfSheep; //static members
+        public float spaceAvailable; //non static members
+        static void Main(string[] args)
+        {
+
+            Components components = new Components(); //instantiate with variable
+            components.printMessages(1);
+            nrOfSheep = Convert.ToInt32(Console.ReadLine());
+            new Components().printMessages(2); //instantiate inline            
+            components.printMessages(3);
+            components.printMessages(4);
+            Console.WriteLine("Try Again: \n");
+            Main(args);
+        }
+    }
+}
